96ffa7879150f888363eca5b918e89f8dee12ef5
Jonathan McLatcher <jdmclatcher@gmail.com>
2019-12-06 09:16:55 -0500

    added day and week fetching of stocks

--- StockViewer/main.py
+++ StockViewer/main.py
@@ -5,4 +5,13 @@
 
 from stocks import *
 
+# functional - TODO needs styling
 printData(daily("MSFT"))
+
+# functional - TODO needs styling
+# printData(weekly("MSFT"))
+
+# TODO - needs work
+# printData(monthly("MSFT"))
+
+# showChart(weekly("AMZN"), "LE EPIC CHORT")
--- StockViewer/stocks.py
+++ StockViewer/stocks.py
@@ -10,27 +10,47 @@ from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 import json
 
+# import datetime
+
 # use api key to create ref to timeseries
 ts = TimeSeries(key="B664X0BHILI87QRE", output_format="pandas")
 
 
 # gets stock results from current day
-def daily(_symbol):
-    return ts.get_quote_endpoint(symbol=_symbol)
+def daily(symbol):
+    # TODO - need to return "5. price" and "10. change percent" column
+    data = ts.get_quote_endpoint(symbol)
+    return data[0]["05. price"]
 
 
-def weekly(_symbol):
-    data, meta_data = ts.get_daily(symbol=_symbol, outputsize="compact")
-    # TODO - edit data to only have last 7 days
-    return data
+def weekly(symbol):
+    data, meta_data = ts.get_daily(symbol, outputsize="compact")
+    # only get data from last 7 days - close price
+    # TODO get close value (4) for each day, week percent, and year to date
+    newData = []
+    y = 0
+    for item in data["4. close"]:
+        if y == 7:
+            break
+        newData.append(item)
+        y = y + 1
+    return newData
 
 
-def monthly(_symbol):
-    return ts.get_monthly(symbol=_symbol)
+# last 30 days - close price
+def monthly(symbol):
+    newData = []
+    y = 0
+    for item in data["4. close"]:
+        if y == 30:
+            break
+        newData.append(item)
+        y = y + 1
+    return newData
 
 
 # TODO - add more variables and customization to chart
-# currenltly doesnt work with daily
+# currently doesn't work with daily
 def showChart(_data, _chartTitle):
     _data["4. close"].plot()
     plt.title(_chartTitle)
