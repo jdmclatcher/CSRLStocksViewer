f349df2a93ea40f041505d65627a55cfbade8004
Jonathan McLatcher <jdmclatcher@gmail.com>
2019-12-08 13:28:28 -0500

    added user input

--- StockViewer/main.py
+++ StockViewer/main.py
@@ -1,14 +1,53 @@
-# Jonathan McLatcher
 # Embedded Programming
 # Stock Viewer Python
 # main.py
 
 from stocks import *
 
-# TODO - user input for symbol and what data to show
 
-daily("MSFT")
-print()
-weekly("MSFT")
-print()
-monthly("MSFT")
+def main():
+    # Alex Eyerly
+    # Gets the company and type of summary to provide
+    stockSelect = input(
+        "What company would you like to see? Please enter the abbreviated symbol for the company. "
+    ).upper()
+    option = input(
+        "Would you like to see the daily, weekly, or monthly summary? Use D, W, or M. "
+    ).upper()
+    # Daily report
+    if option == "D":
+        # Makes sure company inputted was valid
+        try:
+            daily(stockSelect)
+
+        except:
+            print(
+                "Something went wrong. Perhaps you mistyped the stock code or are sending requests too frequently?"
+            )
+
+    elif option == "W":
+        # Makes sure company inputted was valid
+        try:
+            weekly(stockSelect)
+
+        except:
+            print(
+                "Something went wrong. Perhaps you mistyped the stock code or are sending requests too frequently?"
+            )
+
+    elif option == "M":
+        # Makes sure company inputted was valid
+        try:
+            monthly(stockSelect)
+
+        except:
+            print(
+                "Something went wrong. Perhaps you mistyped the stock code or are sending requests too frequently?"
+            )
+    # User inputted an incorrect summary option
+    else:
+        print("Sorry, that's not a recognized command.")
+
+
+if (__name__) == "__main__":
+    main()
